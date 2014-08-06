45da94267545d8a6da77a4ea156feb830700d9da
Stefan Kovachev <st.kovachev@gmail.com>
2014-08-06 16:38:47 +0300

    Initial source commit including simple demo

---
+++ src/message-box/message-box.js
@@ -0,0 +1,148 @@
+﻿(function(angular)
+{
+    'use strict';
+
+    var directives = angular.module('whWidgets.message-box.directives', []);
+    var services = angular.module('whWidgets.message-box.services', []);
+
+    angular.module(
+        'whWidgets.message-box',
+        [
+            'whWidgets.message-box.directives',
+            'whWidgets.message-box.services'
+        ]
+    );
+
+    services.factory('MessageBoxService', [
+        '$log',
+        function($log)
+        {
+            // Private
+
+            var _messageBox = null;
+
+            // Public
+            var _public = {
+                messageBox: function()
+                {
+                    return _messageBox;
+                },
+
+                showMessageBox: function(messageBox)
+                {
+                    if (typeof messageBox.title !== "string")
+                        throw new Error('Message box is missing a valid "title" property.');
+
+                    if (typeof messageBox.content !== "string")
+                        messageBox.content = title;
+
+                    var button;
+                    if (!angular.isArray(messageBox.buttons))
+                    {
+                        button = {
+                            label: 'Close',
+                            title: 'Close this message.',
+                            css: 'btn-default',
+                            onClick: _public.closeMessageBox
+                        };
+
+                        messageBox.buttons = [];
+                        messageBox.buttons.push(button);
+                    }
+                    else
+                    {
+                        var buttons = messageBox.buttons,
+                            len = buttons.length,
+                            i;
+
+                        messageBox.buttons = [];
+
+                        for (i = 0; i < len; i++)
+                        {
+                            button = buttons[i];
+
+                            if (typeof button.label !== "string")
+                            {
+                                $log.error('Message box button is missing a valid "label" property.');
+                                continue;
+                            }
+
+                            if (typeof button.title !== "string")
+                                button.title = button.label;
+
+                            if (typeof button.css !== "string")
+                                button.css = 'btn-default';
+
+                            messageBox.buttons.push(button);
+                        }
+                    }
+
+                    messageBox.buttons = messageBox.buttons.reverse();
+
+                    _messageBox = messageBox;
+                },
+
+                closeMessageBox: function(messageBox, value)
+                {
+                    // Clear message box object (releases UI)
+                    _messageBox = null;
+
+                    // Get button index
+                    var ind = (typeof value === "number") ? value : messageBox.buttons.indexOf(value);
+
+                    // Check if index is in bounds
+                    if (ind < 0 || ind >= messageBox.buttons.length)
+                    {
+                        if (typeof messageBox.onClose === "function")
+                            messageBox.onClose(messageBox);
+
+                        return;
+                    }
+
+                    // Get button
+                    var button = messageBox.buttons[ind];
+
+                    if (typeof button.onClick === "function")
+                        button.onClick(messageBox, button);
+
+                    if (typeof messageBox.onClose === "function")
+                        messageBox.onClose(messageBox, button);
+                }
+            };
+
+            return _public;
+        }
+    ]);
+
+    directives.directive('whMessageBox', [
+            'MessageBoxService',
+            function(MessageBoxService)
+            {
+                return {
+                    restrict: 'EA',
+                    transclude: true,
+                    templateUrl: 'src/message-box/message-box.html',
+                    scope: {},
+                    link: function($scope)
+                    {
+                        $scope.messageBox = null;
+
+                        $scope.close = function(messageBox, button)
+                        {
+                            MessageBoxService.closeMessageBox(messageBox, button);
+                        };
+
+                        $scope.$watch(
+                            MessageBoxService.messageBox,
+                            function(messageBox)
+                            {
+                                $scope.messageBox = messageBox || null;
+                            },
+                            true
+                        );
+                    }
+                };
+            }
+        ]
+    );
+})(angular);
---
+++ src/notifications/notifications.js
@@ -0,0 +1,187 @@
+﻿(function(angular)
+{
+    'use strict';
+
+    var directives = angular.module('whWidgets.notifications.directives', []);
+    var services = angular.module('whWidgets.notifications.services', []);
+
+    angular.module(
+        'whWidgets.notifications',
+        [
+            'whWidgets.notifications.directives',
+            'whWidgets.notifications.services'
+        ]
+    );
+
+    services.factory('NotificationsService', [
+        '$timeout',
+        function($timeout)
+        {
+            // Private
+
+            var _notifications = [];
+
+            // Public
+
+            var _public = {
+                notifications: function()
+                {
+                    return _notifications;
+                },
+
+                showNotification: function(notification)
+                {
+                    if (typeof notification.title !== "string")
+                        throw new Error('Notification is missing a valid "title" property.');
+
+                    if (typeof notification.content !== "string")
+                        notification.content = title;
+
+                    if (typeof notification.type !== "string")
+                        notification.type = 'info';
+
+                    if (typeof notification.timeout !== "number")
+                        notification.timeout = 0;
+
+                    if (typeof notification.icon !== "string")
+                    {
+                        switch (notification.type)
+                        {
+                            case 'success':
+                                notification.icon = 'fa-check-circle';
+                                break;
+
+                            case 'warning':
+                                notification.icon = 'fa-exclamation-triangle';
+                                break;
+
+                            case 'danger':
+                                notification.icon = 'fa-times-circle';
+                                break;
+
+                            default:
+                                notification.icon = 'fa-exclamation-circle';
+                                break;
+                        }
+                    }
+
+                    notification.active = true;
+
+                    for (var i = 0; i < _notifications.length; i++)
+                        _notifications[i].active = false;
+
+                    _notifications.unshift(notification);
+
+                    if (notification.timeout > 0)
+                        $timeout(function() { _public.closeNotification(notification); }, notification.timeout);
+                },
+
+                closeNotification: function(value)
+                {
+                    // Get notification index
+                    var ind = (typeof value === "number") ? value : _notifications.indexOf(value);
+
+                    // Check if index is in bounds
+                    if (ind < 0 || ind >= _notifications.length)
+                        return;
+
+                    // Get notification
+                    var notification = _notifications[ind];
+
+                    // Remove notification from array
+                    _notifications.splice(ind, 1);
+
+                    // If this is the active notification set a new one
+                    if (notification.active && _notifications.length > 0)
+                    {
+                        if (--ind < 0)
+                            ind = 0;
+
+                        _notifications[ind].active = true;
+                    }
+
+                    // Call onClose callback (if any)
+                    if (typeof notification.onClose === "function")
+                        notification.onClose(notification);
+                },
+
+                setActiveNotification: function(value)
+                {
+                    // Get notification index and length
+                    var ind = (typeof value === "number") ? value : _notifications.indexOf(value),
+                        len = _notifications.length;
+
+                    // Check if index is in bounds
+                    if (ind < 0 || ind >= len)
+                        return;
+
+                    // Get notification
+                    var notification = _notifications[ind];
+
+                    // Clear notifications active flag
+                    for (var i = 0; i < len; i++)
+                        _notifications[i].active = false;
+
+                    // Set the new active notification
+                    _notifications[ind].active = true;
+
+                    // Call onActive callback (if any)
+                    if (typeof notification.onActive === "function")
+                        notification.onActive(notification);
+                }
+            };
+
+            return _public;
+        }
+    ]);
+
+    directives.directive('whNotifications', [
+            'NotificationsService',
+            function(NotificationsService)
+            {
+                return {
+                    restrict: 'EA',
+                    transclude: true,
+                    templateUrl: 'src/notifications/notifications.html',
+                    scope: {},
+                    link: function($scope)
+                    {
+                        $scope.notifications = [];
+
+                        $scope.getStyle = function(notification)
+                        {
+                            var style = {};
+
+                            if (notification.active)
+                                style['z-index'] = '10000';
+
+                            if (typeof notification.color === "string")
+                                style['background-color'] = notification.color;
+
+                            return style;
+                        };
+
+                        $scope.setActive = function(notification)
+                        {
+                            NotificationsService.setActiveNotification(notification);
+                        };
+
+                        $scope.close = function(notification)
+                        {
+                            NotificationsService.closeNotification(notification);
+                        };
+
+                        $scope.$watch(
+                            NotificationsService.notifications,
+                            function(notifications)
+                            {
+                                $scope.notifications = notifications || [];
+                            },
+                            true
+                        );
+                    }
+                };
+            }
+        ]
+    );
+})(angular);
